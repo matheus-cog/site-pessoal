d06050d8a7781daaaf5fbe8b9150f3c8f3c0b4c5
Matheus Guedes <matheus.cog@hotmail.com>
2022-07-08 01:59:07 -0300

    new file:   README.md 	new file:   assets/css/styles.css 	new file:   assets/img/favicon.png 	new file:   assets/img/perfil.png 	new file:   assets/js/main.js 	new file:   assets/js/scrollreveal.min.js 	new file:   index.html

---
+++ README.md
@@ -0,0 +1,7 @@
+# matheusguedes.com
+## [Acesse!](https://matheusguedes.com)
+### Este é o projeto presente em meu site pessoal.
+
+- Contém animações feitas com CSS.
+- Inclui modo claro e escuro.
+- Feito com a metodologia Mobile First, depois desktop.
---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : iconTheme;
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+function getTimeDifference(date){
+    let currentDate = new Date();
+    let referenceDate = new Date(date);
+    let resultYears = currentDate.getFullYear() - referenceDate.getFullYear();
+    let resultMonths = (currentDate.getMonth() - referenceDate.getMonth()) / 12;
+    return (resultYears + resultMonths).toFixed(1);
+}
+
+dev.innerText = getTimeDifference("2021/02/01");
+tech.innerText = getTimeDifference("2006/11/28");
+drummer.innerText = getTimeDifference("2017/05/05");
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+sr.reveal(`.profile_border`);
+sr.reveal(`.profile_name`, { delay: 500 });
+sr.reveal(`.profile_profession`, { delay: 600 });
+sr.reveal(`.profile_social`, { delay: 700 });
+sr.reveal(`.profile_info-group`, { interval: 100, delay: 700 });
+sr.reveal(`.filters_content`, { delay: 800 });
+sr.reveal(`.filters`, { delay: 900 });
